1575dfd2a7619909014f9e4778d99cb9abcc0151
Prasad Magdum <magdumprasad194@gmail.com>
2025-06-22 13:58:16 +0530

    update files

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
 
-    mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/bookmyturf', {
+    mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     })
